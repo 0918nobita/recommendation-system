cc7bf3e413ae812579af1bdf185f4b60071b954c
Kodai Matsumoto <nobita.0918@gmail.com>
2022-10-17 21:56:01 +0900

    :recycle: Do refactor

--- LinearAlgebra.fsx
+++ LinearAlgebra.fsx
@@ -44,57 +44,90 @@ type Vector<'T> =
         |> Vec
 
 type Matrix<'T> =
-    | Matrix of list<list<'T>>
+    | Matrix of numRows: int * numCols: int * rows: list<list<'T>>
 
-    static member inline numRows< ^t> (mat: Matrix< ^t>) : int =
-        match mat with
-        | Matrix rows -> List.length rows
+    override this.ToString() =
+        match this with
+        | Matrix (_, _, rows) -> sprintf "%A" rows
 
-    static member inline numCols< ^t> (mat: Matrix< ^t>) : int =
-        match mat with
-        | Matrix rows -> List.length (List.head rows)
+    static member inline rows (Matrix (_, _, rows): Matrix< ^t>) : list<list< ^t>> =
+        rows
 
-    static member inline shape< ^t> (mat: Matrix< ^t>) : int * int =
+    static member inline columns (Matrix (_, numCols, rows) : Matrix< ^t>) : list<list< ^t>> =
+        seq {
+            for i in 0 .. numCols - 1 ->
+                List.map (List.item i) rows
+        }
+        |> List.ofSeq
+
+    static member inline numRows (Matrix (numRows, _, _)) : int =
+        numRows
+
+    static member inline numCols (Matrix (_, numCols, _)) : int =
+        numCols
+
+    static member inline shape (mat: Matrix< ^t>) : int * int =
         Matrix.numRows< ^t> mat, Matrix.numCols< ^t> mat
 
-    static member inline sameShape< ^t> (matA: Matrix< ^t>) (matB: Matrix< ^t>) : bool =
-        Matrix.shape< ^t> matA = Matrix.shape< ^t> matB
+    static member inline sameShape (a: Matrix< ^t>) (b: Matrix< ^t>) : bool =
+        Matrix.shape< ^t> a = Matrix.shape< ^t> b
 
     static member inline (+) (a: Matrix< ^t>, b: Matrix< ^t>) : Matrix< ^t> =
-        if not (Matrix.sameShape< ^t> a b) then failwith "Dimension mismatch"
+        if not (Matrix.sameShape<'t> a b) then failwith "Dimension mismatch"
 
-        let (Matrix a) = a
-        let (Matrix b) = b
+        let (numRows, numCols) = Matrix.shape< ^t> a
 
-        List.map2 (List.map2 (+)) a b
-        |> Matrix
+        let a = Matrix.rows< ^t> a
+        let b = Matrix.rows< ^t> b
+
+        let rows = List.map2 (List.map2 (+)) a b
+        Matrix (numRows, numCols, rows)
 
     static member inline (-) (a: Matrix< ^t>, b: Matrix< ^t>) : Matrix< ^t> =
         if not (Matrix.sameShape< ^t> a b) then failwith "Dimension mismatch"
 
-        let (Matrix a) = a
-        let (Matrix b) = b
+        let (numRows, numCols) = Matrix.shape< ^t> a
 
-        List.map2 (List.map2 (-)) a b
-        |> Matrix
+        let a = Matrix.rows< ^t> a
+        let b = Matrix.rows< ^t> b
+
+        let rows = List.map2 (List.map2 (-)) a b
+        Matrix (numRows, numCols, rows)
 
     static member inline (*) (a: Matrix< ^t>, b: Matrix< ^t>) : Matrix< ^t> =
-        let aRows = Matrix.numRows< ^t> a
-        let bCols = Matrix.numCols< ^t> b
-        if aRows <> bCols then failwith "Dimension mismatch"
+        let aCols = Matrix.numCols< ^t> a
+        let bRows = Matrix.numRows< ^t> b
+        if aCols <> bRows then failwith "Dimension mismatch"
 
-        let (Matrix a) = a
-        let (Matrix b) = b
+        let rows =
+            seq {
+                for aRow in Matrix.rows< ^t> a ->
+                    List.ofSeq <| seq {
+                        for bCol in Matrix.columns< ^t> b ->
+                            List.sum <| List.map2 (*) aRow bCol
+                    }
+            }
+            |> List.ofSeq
+        
+        Matrix (Matrix.numRows< ^t> a, Matrix.numCols< ^t> b, rows)
 
-        seq {
-            for aRow in a do
-                yield List.ofSeq <| seq {
-                    for iCol in 0 .. (List.length (List.head b) - 1) do
-                        let bCol = b |> List.map (fun bRow -> bRow.[iCol])
-                        yield List.sum <| List.map2 (*) aRow bCol
-                }
-        }
-        |> List.ofSeq
-        |> Matrix
+module Matrix =
+    let inline make(rows: list<list<'t>>) : Matrix<'t> =
+        let numRows = List.length rows
+        let numCols = List.length (List.head rows)
+        Matrix (numRows, numCols, rows)
 
-printfn "%A" <| Matrix [[1; 3]; [-3; 4]] * Matrix [[1; 9]; [9; 3]] // [[28; 18]; [33; -15]]
+(Matrix.make [[1; 3]; [-3; 4]])
+* (Matrix.make [[1; 9]; [9; 3]])
+|> printfn "%O" // [[28; 18]; [33; -15]]
+
+(Matrix.make [[1; 0]; [4; 1]; [-1; 2]])
+* (Matrix.make [[1; 3]; [2; -1]])
+|> printfn "%O" // [[1; 3]; [6; 11]; [3; -5]]
+
+let a = Matrix.make [[1; 0]; [4; 1]; [-1; 2]]
+let b = Matrix.make [[1; 3]; [2; -1]; [0; 0]]
+try
+    printfn "%O" <| a * b
+with
+    _ -> eprintfn "Dimension mismatch: %O * %O" a b
